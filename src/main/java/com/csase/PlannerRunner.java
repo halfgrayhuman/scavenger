64547b982b2bd449d32a17fad99fcb47fec4bc7f
halfgrayhuman <138070452+halfgrayhuman@users.noreply.github.com>
2024-05-10 10:57:47 -0400

    FIXED LESSSS GOOOOOOO

--- src/main/java/com/csase/PlannerRunner.java
+++ src/main/java/com/csase/PlannerRunner.java
@@ -5,9 +5,11 @@ public class PlannerRunner {
 
         // Create a new student
         Student jada = new Student();
+        Student chris = new Student();
 
         // Gets the student's choice until the student exits
         jada.getChoice();
+        chris.getChoice();
 
     }
 }
